ee099b22840ff9e64cb55ad5e3ef85ea8fb24094
Svale Fossåskaret <svale@3by5.no>
2016-06-16 12:40:47 +0200

    Fixes path to data examples

--- bin/watch.sh
+++ bin/watch.sh
@@ -6,7 +6,7 @@ postcss -u autoprefixer -w -o example/css/main.css src/css/main.css &
 onchange 'src/index.html' -- cp src/index.html example/ &
 onchange 'src/js/vendor' -v -- cp -a src/js/vendor/. example/js/vendor/ &
 onchange 'src/css/vendor' -v -- cp -a src/css/vendor/. example/css/vendor/ &
-onchange 'src/data' -v -- cp -a src/data/. example/css/vendor/ &
+onchange 'src/data' -v -- cp -a src/data/. example/data/ &
 
 watchify src/js/main.js -o example/js/main.js --debug --verbose &
 
